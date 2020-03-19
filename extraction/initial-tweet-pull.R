f5837e17ef5346f8e8567f0274a6983c53b2a0db
hendersontrent <trent.henderson1@outlook.com>
2020-03-19 17:43:53 +1000

    Started writing initial twitter scrape

---
+++ extraction/initial-tweet-pull.R
@@ -0,0 +1,14 @@
+#---------------------------------------
+# This script sets out to pull tweets
+# from Australian universities
+#
+# NOTE: This script requires setup.R and  
+# twitter-initialisation.R to have been
+# run first
+#---------------------------------------
+
+#---------------------------------------
+# Author: Trent Henderson, 19 March 2020
+#---------------------------------------
+
+
